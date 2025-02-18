6eb4685e8b43829f86406a6c06276ece80dc78a2
zkondolewicz <zkondolewicz@nvidia.com>
2025-02-18 10:26:09 +0100

    Update + tmux

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "cuda",
+	callback = function()
+		vim.bo.commentstring = "// %s"
+	end,
+})
